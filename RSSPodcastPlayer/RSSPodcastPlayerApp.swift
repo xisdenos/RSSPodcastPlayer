7fe1025d2be4c17e3c1afa4f91c81424923d6fc5
Lucas Pinto <lucaspinto.andrade@gmail.com>
2024-12-07 16:04:36 -0300

    [FEATURE] basic new project files

---
+++ RSSPodcastPlayer/RSSPodcastPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSSPodcastPlayerApp.swift
+//  RSSPodcastPlayer
+//
+//  Created by Lucas Pinto on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RSSPodcastPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
