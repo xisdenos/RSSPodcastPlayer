8fe68a1623485ef13f63cbf129f7ad5239dc25f0
Lucas Pinto <lucaspinto.andrade@gmail.com>
2024-12-09 17:10:10 -0300

    [FEATURE] basic CacheManager

---
+++ RSSPodcastPlayer/Data/Cache/CacheManager.swift
@@ -0,0 +1,54 @@
+//
+//  CacheManager.swift
+//  RSSPodcastPlayer
+//
+//  Created by Lucas Pinto on 09/12/24.
+//
+
+import Foundation
+
+protocol AnyObjectCacheManagerProtocol {
+    func setAnyObject(_ object: AnyObject, forKey key: String)
+    func getAnyObject(forKey key: String) -> AnyObject?
+}
+
+final class CacheManager: AnyObjectCacheManagerProtocol {
+    
+    static let shared = CacheManager()
+    private let fileManager: FileManager
+    private let cacheDirectoryURL: URL
+    
+    private init?(fileManager: FileManager = .default) {
+        self.fileManager = fileManager
+        let cacheDirectory = fileManager.urls(for: .cachesDirectory, in: .userDomainMask).first
+        
+        guard let cacheDirectory else { return nil }
+        self.cacheDirectoryURL = cacheDirectory.appendingPathComponent("CustomCache", isDirectory: true)
+        
+        checkAndCreateIfDirectoryNotExists(url: cacheDirectoryURL)
+    }
+    
+    
+    func setAnyObject(_ object: AnyObject, forKey key: String) {
+        <#code#>
+    }
+    
+    func getAnyObject(forKey key: String) -> AnyObject? {
+        <#code#>
+    }
+}
+
+//Todo: create a CacheManagerValidator class to validate this
+
+extension CacheManager {
+    
+    private func checkAndCreateIfDirectoryNotExists(url: URL) {
+        if !fileManager.fileExists(atPath: url.path) {
+            createDirectoryIfNotExists(url: url)
+        }
+    }
+    
+    private func createDirectoryIfNotExists(url: URL) {
+        try? fileManager.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+    }
+}
---
+++ RSSPodcastPlayer/Feature/RSSPodcastHome/RSSPodcastHomeViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  RSSPodcastHomeViewModel.swift
+//  RSSPodcastPlayer
+//
+//  Created by Lucas Pinto on 07/12/24.
+//
+
+class RSSPodcastHomeViewModel {
+    
+}
