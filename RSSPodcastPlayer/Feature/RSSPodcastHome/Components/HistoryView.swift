1545ad23953ea983cc324d0df36d9c335b691512
Lucas Pinto <lucaspinto.andrade@gmail.com>
2024-12-10 16:05:55 -0300

    [FEATURE] New Custom components to RSSHomeView

--- RSSPodcastPlayer/Feature/RSSPodcastHome/Components/HistoryView.swift
+++ RSSPodcastPlayer/Feature/RSSPodcastHome/Components/HistoryView.swift
@@ -8,28 +8,49 @@ import SwiftUI
 
 struct HistoryView: View {
     
-    @StateObject var viewModel: RSSPodcastHomeViewModel
+    @ObservedObject var viewModel: RSSPodcastHomeViewModel
     
     var body: some View {
-            Group {
-                if viewModel.checkShowHistory() {
-                    VStack(alignment: .leading) {
-                        ForEach(viewModel.urlStringList, id: \.self) { url in
-                            Text(url)
-                                .padding(5)
-                                .background(Color.gray.opacity(0.2))
-                                .cornerRadius(5)
-                                .onTapGesture {
-                                    viewModel.url = url
-                                    viewModel.showHistory = false
-                                }
-                        }
+        VStack(alignment: .center, spacing: 20) {
+            Text("Last Podcasts")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+                .padding(.top)
+            
+            ForEach(viewModel.urlStringList, id: \.self) { url in
+                Button(action: {
+                    viewModel.url = url
+                    viewModel.loadParsedPodcast()
+                }) {
+                    HStack {
+                        Image(systemName: "link.circle.fill")
+                            .foregroundColor(.red)
+                            .font(.title2)
+                        
+                        Text(url)
+                            .font(.body)
+                            .lineLimit(1)
+                            .truncationMode(.middle)
+                            .foregroundColor(.primary)
                     }
-                    .padding(.top, 5)
+                    .padding(.vertical, 12)
+                    .padding(.horizontal)
+                    .frame(maxWidth: .infinity, alignment: .leading)
                     .background(Color.white)
-                    .cornerRadius(8)
+                    .cornerRadius(10)
                     .shadow(radius: 5)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray.opacity(0.2), lineWidth: 1)
+                    )
                 }
+                .buttonStyle(PlainButtonStyle())
             }
         }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(15)
+        .padding(.horizontal)
+    }
 }
