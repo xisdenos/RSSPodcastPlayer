afa17263f556c95bbe96ea3625ce334f87c9d56f
Lucas Pinto <lucaspinto.andrade@gmail.com>
2024-12-11 18:42:36 -0300

    [UPDATE] app components color pallet

--- RSSPodcastPlayer/Feature/RSSPodcastHome/Components/HistoryView.swift
+++ RSSPodcastPlayer/Feature/RSSPodcastHome/Components/HistoryView.swift
@@ -11,45 +11,50 @@ struct HistoryView: View {
     @ObservedObject var viewModel: RSSPodcastHomeViewModel
     
     var body: some View {
-        VStack(alignment: .center, spacing: 20) {
+        VStack(alignment: .center) {
             Text("Last Podcasts")
                 .font(.title2)
                 .fontWeight(.semibold)
                 .foregroundColor(.white)
                 .padding(.top)
             
-            ForEach(viewModel.urlStringList, id: \.self) { url in
-                Button(action: {
-                    viewModel.url = url
-                    viewModel.loadParsedPodcast()
-                }) {
-                    HStack {
-                        Image(systemName: "link.circle.fill")
-                            .foregroundColor(.red)
-                            .font(.title2)
-                        
-                        Text(url)
-                            .font(.body)
-                            .lineLimit(1)
-                            .truncationMode(.middle)
-                            .foregroundColor(.primary)
+            VStack(spacing: 4) {
+                ForEach(viewModel.urlStringList, id: \.self) { url in
+                    Button(action: {
+                        viewModel.url = url
+                        viewModel.loadParsedPodcast()
+                    }) {
+                        HStack {
+                            Image(systemName: "link.circle.fill")
+                                .foregroundColor(.red)
+                                .font(.title2)
+                            
+                            Text(url)
+                                .font(.body)
+                                .lineLimit(1)
+                                .truncationMode(.middle)
+                                .foregroundColor(.primary)
+                        }
+                        .padding(.vertical, 12)
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray.opacity(0.2), lineWidth: 1)
+                        )
                     }
-                    .padding(.vertical, 12)
-                    .padding(.horizontal)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(Color.white)
-                    .cornerRadius(10)
-                    .shadow(radius: 5)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.gray.opacity(0.2), lineWidth: 1)
-                    )
+                    .buttonStyle(PlainButtonStyle())
                 }
-                .buttonStyle(PlainButtonStyle())
+                
+                CustomButton(buttonText: "Apagar Historico", foregroundColor: Color.red) {
+                    viewModel.clearSavedURls()
+                }
             }
         }
-        .padding()
-        .background(LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]), startPoint: .top, endPoint: .bottom))
+        .background(Color.primaryBlue)
         .cornerRadius(15)
         .padding(.horizontal)
     }
