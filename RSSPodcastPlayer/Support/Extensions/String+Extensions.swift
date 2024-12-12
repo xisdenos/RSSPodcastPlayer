d9990512368401dc3fafe54c2950781a9b57720a
Lucas Pinto <lucaspinto.andrade@gmail.com>
2024-12-12 20:18:07 -0300

    [FEATURE] New string formatters

--- RSSPodcastPlayer/Support/Extensions/String+Extensions.swift
+++ RSSPodcastPlayer/Support/Extensions/String+Extensions.swift
@@ -9,10 +9,28 @@ import Foundation
 extension String {
     
     func stringToURL() -> URL? {
-            return URL(string: self)
+        return URL(string: self)
     }
     
-    func toDouble() -> Double? {
-        return Double(self)
+    func stringToSeconds() -> Double? {
+        let components = self.split(separator: ":")
+        guard components.count == 3,
+              let hours = Double(components[0]),
+              let minutes = Double(components[1]),
+              let seconds = Double(components[2]) else {
+            return nil
+        }
+        
+        return (hours * 3600) + (minutes * 60) + seconds
     }
+    
+    func removingHTMLTags() -> String {
+       do {
+           let regex = try NSRegularExpression(pattern: "<.*?>", options: [])
+           let range = NSRange(location: 0, length: self.utf16.count)
+           return regex.stringByReplacingMatches(in: self, options: [], range: range, withTemplate: "")
+       } catch {
+           return self
+       }
+   }
 }
