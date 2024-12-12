e32425019246436d98e2cb4315c6d5a894d5bf39
Lucas Pinto <lucaspinto.andrade@gmail.com>
2024-12-12 18:11:35 -0300

    [FIX] Fix audioplayer not playing any audio

--- RSSPodcastPlayer/Feature/RSSPlayer/AudioPlayer/AudioPlayer.swift
+++ RSSPodcastPlayer/Feature/RSSPlayer/AudioPlayer/AudioPlayer.swift
@@ -5,56 +5,100 @@
 //  Created by Lucas Pinto on 11/12/24.
 //
 
-import Foundation
+import SwiftUI
+import Combine
 import AVFoundation
 
 protocol AudioPlayerProtocol {
-    func play(url: URL?) -> AudioPlayerError?
+    func play(url: URL?)
+    func changeAudio(url: URL?)
     func pause()
-    func next() -> AudioPlayerError?
-    func previous() -> AudioPlayerError?
 }
 
-class AudioPlayer: ObservableObject, AudioPlayerProtocol {
+class AudioPlayer: NSObject, AudioPlayerProtocol, ObservableObject {
+    
+    // MARK: Private variables
+    
     private var player: AVPlayer
-    private var audioSourcePodcast: Podcast
     private var currentEpisode: Episode
+    private var timeObserver: Any?
+    
+    // MARK: Public variables
+    
+    @Published var currentTime: TimeInterval = 0
+    @Published var isPaused: Bool = false
+    
+    // MARK: Init
 
-    init(audioSourcePodcast: Podcast, currentEpisode: Episode) {
-        self.audioSourcePodcast = audioSourcePodcast
+    init(currentEpisode: Episode) {
         self.currentEpisode = currentEpisode
         self.player = AVPlayer()
     }
     
-    private func findEpisodeIndex() -> Int {
-        guard let index = audioSourcePodcast.episodes.firstIndex(of: currentEpisode) else { return 0 }
-        return index
+    deinit {
+        removeAVPlayerEntities()
     }
+    
+    // MARK: Private funcs
+    
+    func removeAVPlayerEntities() {
+        player.pause()
+        player.replaceCurrentItem(with: nil)
+        isPaused = true
+        currentTime = 0
+        if let observer = timeObserver {
+            player.removeTimeObserver(observer)
+        }
+    }
+    
+    private func observePlayerEvents() {
+        player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 600), queue: .main) { [weak self] time in
+            self?.currentTime = CMTimeGetSeconds(time)
+        }
+    }
+    
+    private func setupPlayerPreferences(player: AVPlayer) {
+        player.allowsExternalPlayback = true
+        player.appliesMediaSelectionCriteriaAutomatically = true
+        player.automaticallyWaitsToMinimizeStalling = true
+        player.volume = 1.0
+    }
+    
+    // MARK: Public funcs
 
-    func play(url: URL?) -> AudioPlayerError? {
-        guard let url else { return .invalidTrackURL }
-        let player = AVPlayer(url: url)
+    func play(url: URL?) {
+        guard let url else {
+//            errorCaller(error: AudioPlayerError.invalidTrackURL)
+            return
+        }
+        
+        if player.currentItem == nil {
+            player = AVPlayer(playerItem: AVPlayerItem(url: url))
+            setupPlayerPreferences(player: player)
+            observePlayerEvents()
+        }
+        if player.currentItem != nil && !isPaused {
+            player.replaceCurrentItem(with: AVPlayerItem(url: url))
+        }
+        
         player.play()
-        return nil
     }
 
     func pause() {
         player.pause()
+        isPaused = true
     }
-
-    func next() -> AudioPlayerError? {
-        let nextIndex = findEpisodeIndex() + 1
-        guard nextIndex < audioSourcePodcast.episodes.count else { return .noNextTrack }
-        let nextEpisodeURL = audioSourcePodcast.episodes[nextIndex].enclosure?.url
-        play(url: nextEpisodeURL)
-        return nil
-    }
-
-    func previous() -> AudioPlayerError? {
-        let previousIndex = findEpisodeIndex() - 1
-        guard previousIndex >= 0 else { return .noPreviousTrack }
-        let previousEpisodeURL = audioSourcePodcast.episodes[previousIndex].enclosure?.url
-        play(url: previousEpisodeURL)
-        return nil
+    
+    func changeAudio(url: URL?) {
+        removeAVPlayerEntities()
+        play(url: url)
     }
 }
+
+//// MARK: Delegate extension
+//
+//extension AudioPlayer: AVAudioPlayerDelegate {
+//    func audioPlayerDidChangePosition(_ player: AVAudioPlayer) {
+//        currentTime = player.currentTime
+//    }
+//}
